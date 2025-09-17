ea1c9c6c98278d4fb32dd94df506b14217bf5e65
Godrick1408 <godrick1408@gmail.com>
2025-09-17 19:15:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const mainNav = document.getElementById('main-nav');
     const menuToggle = mainNav.querySelector('.menu-toggle');
-    const menuItems = mainNav.querySelector('.menu-items');
     
     // Функція, що перемикає клас для розгортання/згортання меню
     function toggleMenu() {
         mainNav.classList.toggle('menu-expanded');
-        mainNav.classList.toggle('menu-collapsed');
     }
 
     // Додаємо обробник кліків для мобільного меню
     if (window.innerWidth <= 768) {
         menuToggle.addEventListener('click', toggleMenu);
+    } else {
+        mainNav.classList.remove('menu-expanded');
     }
 
     // Прибираємо згортання/розгортання на комп'ютері
     window.addEventListener('resize', () => {
       if (window.innerWidth > 768) {
-        mainNav.classList.remove('menu-expanded', 'menu-collapsed');
-        menuItems.style.maxHeight = 'none';
-      } else {
-        mainNav.classList.add('menu-collapsed');
-        menuItems.style.maxHeight = '0';
+        mainNav.classList.remove('menu-expanded');
       }
     });
+
 });
