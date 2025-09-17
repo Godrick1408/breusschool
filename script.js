96782833e1570731046e7c2ea347bd3d3e61e0cc
Godrick1408 <godrick1408@gmail.com>
2025-09-17 19:06:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainNav = document.getElementById('main-nav');
     const menuToggle = mainNav.querySelector('.menu-toggle');
     const menuItems = mainNav.querySelector('.menu-items');
-    const header = document.querySelector('header');
     
-    // Перевіряємо, чи ми на мобільній версії
-    const isMobile = window.innerWidth <= 768;
-
-    // Сховати меню на мобільному, якщо воно було розгорнуте
-    if (isMobile) {
-        menuItems.style.maxHeight = '0';
+    // Функція, що перемикає клас для розгортання/згортання меню
+    function toggleMenu() {
+        mainNav.classList.toggle('menu-expanded');
+        mainNav.classList.toggle('menu-collapsed');
     }
 
-    // Додаємо обробник подій для прокручування сторінки
-    let lastScrollY = window.scrollY;
-
-    window.addEventListener('scroll', () => {
-        const currentScrollY = window.scrollY;
-        
-        if (currentScrollY > header.offsetHeight && currentScrollY > lastScrollY) {
-            // Згортаємо меню при прокручуванні вниз, якщо прокручено нижче хедера
-            mainNav.classList.remove('menu-expanded');
-            mainNav.classList.add('menu-collapsed');
-        } else if (currentScrollY < lastScrollY) {
-            // Розгортаємо меню при прокручуванні вгору
-            mainNav.classList.remove('menu-collapsed');
-            mainNav.classList.add('menu-expanded');
-        }
-
-        lastScrollY = currentScrollY;
-    });
-
     // Додаємо обробник кліків для мобільного меню
-    if (isMobile) {
-        menuToggle.addEventListener('click', () => {
-            mainNav.classList.toggle('menu-expanded');
-            mainNav.classList.toggle('menu-collapsed');
-        });
+    if (window.innerWidth <= 768) {
+        menuToggle.addEventListener('click', toggleMenu);
     }
 
     // Прибираємо згортання/розгортання на комп'ютері
@@ -49,5 +24,4 @@ document.addEventListener('DOMContentLoaded', () => {
         menuItems.style.maxHeight = '0';
       }
     });
-
 });
