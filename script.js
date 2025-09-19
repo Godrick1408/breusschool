5e1795d56db956f5f0d3694b8ae429281e659e55
Godrick1408 <godrick1408@gmail.com>
2025-09-19 18:56:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,62 +2,42 @@ document.addEventListener('DOMContentLoaded', () => {
     const mainNav = document.getElementById('main-nav');
     const menuToggle = mainNav.querySelector('.menu-toggle');
     
-    // Функція, що перемикає клас для розгортання/згортання меню
     function toggleMenu() {
         mainNav.classList.toggle('menu-expanded');
     }
 
-    // Додаємо обробник кліків для мобільного меню
     if (window.innerWidth <= 768) {
         menuToggle.addEventListener('click', toggleMenu);
     } else {
         mainNav.classList.remove('menu-expanded');
     }
 
-    // Прибираємо згортання/розгортання на комп'ютері
     window.addEventListener('resize', () => {
       if (window.innerWidth > 768) {
         mainNav.classList.remove('menu-expanded');
       }
     });
 
-    // Обробка випадаючих меню (для мобільного)
     const dropdowns = mainNav.querySelectorAll('.dropdown');
 
     dropdowns.forEach(dropdown => {
         const link = dropdown.querySelector('a');
 
         link.addEventListener('click', (e) => {
-            // Зупиняємо "спливання" кліку
-            e.stopPropagation();
-
-            // Запобігаємо переходу за посиланням тільки на мобільних пристроях
             if (window.innerWidth <= 768) {
+                e.preventDefault();
+
                 const isAlreadyActive = dropdown.classList.contains('active');
 
-                // Закриваємо всі відкриті підменю
-                dropdowns.forEach(otherDropdown => {
-                    otherDropdown.classList.remove('active');
-                });
-                
-                // Якщо меню не було активним, відкриваємо його
-                if (!isAlreadyActive) {
-                    e.preventDefault(); 
+                if (isAlreadyActive) {
+                    dropdown.classList.remove('active');
+                } else {
+                    dropdowns.forEach(otherDropdown => {
+                        otherDropdown.classList.remove('active');
+                    });
                     dropdown.classList.add('active');
                 }
             }
         });
-    });
-    
-    // Додаємо обробник кліків на весь документ, щоб закривати меню, якщо клік був поза ним
-    document.addEventListener('click', (e) => {
-        if (window.innerWidth <= 768) {
-            const isClickInsideMenu = mainNav.contains(e.target);
-            if (!isClickInsideMenu) {
-                dropdowns.forEach(dropdown => {
-                    dropdown.classList.remove('active');
-                });
-            }
-        }
     });
 });
